04d24833e58cbbae25f870653eb156a52fce20cd
AFRID SHAIK <afridayan01@gmail.com>
2024-01-01 05:59:44 +0000

    started Project

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+export default function Home(){
+
+
+
+
+
+
+    return(
+
+        <>
+        <main>
+            <section>
+                <div>
+                    <div>
+                        <div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+
+
+        </>
+    )
+}
